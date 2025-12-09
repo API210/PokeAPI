41838b646e7c5c0be5cecb3a015d94ff1c174627
Hurleyms <hurleyms94@gmail.com>
2025-12-08 19:33:49 -0500

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,7 +1,7 @@
 const fetchBtn = document.getElementById('fetchBtn');
 const randPokeDiv = document.getElementById('randPoke');
 
-fetchBtn = addEventListener("click", () =>{
+fetchBtn.addEventListener("click", () =>{
     const appUrl = "https://pokeapi.co/api/v2/pokemon/ditto";
 
     fetch(appUrl)
@@ -12,4 +12,19 @@ fetchBtn = addEventListener("click", () =>{
         }
         return response.json();
     })
+
+.then(data => {
+    const pokeName = data.name;
+    const pokeAbil = abilities.name;
+    const pokeMoves = moves.move.name;
+console.log("data");
+    randPokeDiv.innerHTML = `<p><strong>Pokemon Name:</strong> ${data.name}</p>
+    <p><strong>Pokemon Ability:</strong> ${abilities.name} C</p>
+    <p><strong>Wind Speed:</strong> ${moves.move.name} km/h</p>`;
+})
+.catch(error =>{
+    randPokeDiv.innerHTML = `
+    <p><strong>Error:</strong> ${error.message}</p>`;
+    console.log(error);
+});
 });
